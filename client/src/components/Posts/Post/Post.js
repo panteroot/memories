8b61fd56aee694bda93bd4e7399157f2c4521b7c
pan <testmail>
2024-01-02 20:19:20 +0800

    added not-found image from own git repo

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -73,7 +73,7 @@ const Post = ({ post }) => {
         <Card className={classes.card} raised elevation={6}>
             <CardMedia 
                     className={classes.media} 
-                    image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} 
+                    image={post.selectedFile || 'https://github.com/panteroot/memories/blob/production/client/src/images/not-found.png'} 
                     title={post.title} 
             />
 
